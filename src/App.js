ac00af4f110d9f34d815b22c8d9d770bcadd28b8
mitchell-msl <mitchell-MSL@users.noreply.github.com>
2024-05-09 20:44:40 -0700

    completed reavt version of quote machine

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import './App.scss';
+
+let apiSrc = "https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
 
 function App() {
+  const [quote, setQuote] = useState("Quote")
+  const [author, setAuthor] = useState("Author")
+  const [randomNumber, setNumber] = useState(0)
+  const [quotesArray, setQuotesArray] = useState(null)
+
+  const fetchQuotes = async (url) => {
+    const response = await fetch(url)
+    const parsedJson = await response.json()
+    setQuotesArray(parsedJson.quotes)
+  }
+
+  useEffect(() => {
+    fetchQuotes(apiSrc)
+  }, [apiSrc])
+
+  //this function updates to a new quote and author at random
+  const newQA = () => {
+      let randomNum =  Math.floor(Math.random() * quotesArray.length);
+      setQuote(quotesArray[randomNum].quote);
+      setAuthor(quotesArray[randomNum].author);
+  }
+
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="quote-box">
+          <h1 id="text">
+            "{quote}"
+          </h1>
+          <h4 id="author">
+            "{author}"
+          </h4>
+          <a id="tweet-quote" href={encodeURI(`http://www.twitter.com/intent/tweet?text=${quote} -{author}`)}>Tweet Quote</a>
+          <button id="new-quote" onClick={() => newQA()}>Next</button>
+        </div>
       </header>
     </div>
   );
